05388f8a5d6a6d1cf8b455a1eaeb56c0913b3ff2
RaW <akram.iiuc.ctg@gmail.com>
2024-05-23 21:50:13 +0600

    updated

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const userController = require("./controllers/user_controllers");
 const flatListController = require("./controllers/flatList_controllers");
 const roommateListController = require("./controllers/roommateList_controllers");
 const wishListController = require("./controllers/wishlist_controllers");
-const aboutController =require("./controllers/about_controller")
-const footerController =require("./controllers/footer_controller")
-const reportController =require("./controllers/report_controller")
+const aboutController = require("./controllers/about_controller");
+const footerController = require("./controllers/footer_controller");
+const reportController = require("./controllers/report_controller");
+const logoController = require("./controllers/logo_controller");
 // Middleware
-app.use(cors({origin: "https://to-lets.netlify.app"}));
+app.use(
+  cors({ origin: ["https://to-lets.netlify.app", "http://localhost:5173"] })
+);
+
 app.use(express.json());
 app.use("/images", express.static("images"));
 require("dotenv").config();
@@ -30,6 +34,7 @@ async function run() {
     app.use("/", aboutController);
     app.use("/", footerController);
     app.use("/", reportController);
+    app.use("/", logoController);
     // Generate random secret key
     const generateSecretKey = () => {
       return crypto.randomBytes(32).toString("hex"); // Generates a random 32-byte hexadecimal string
